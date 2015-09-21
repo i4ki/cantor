7aa2830a3ecf8a77db9587f4816e5b3b43565780
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2015-09-21 01:00:12 -0300

    tests/bitset/test.c: improve tests

--- tests/bitset/test.c
+++ tests/bitset/test.c
@@ -3,18 +3,25 @@
 #include <stdlib.h>
 #include <cantor/cantor.h>
 
-void main(void)
+typedef struct {
+	u8* name;
+	u8 (*fn)(void);
+} TestFn;
+
+TestFn tests[2]; 
+
+u8 testBasicBitSetOneByte()
 {
 	BitSet bset;
 	u32 i = 0;
 	u8 bit = 0;
-	Error err;
+	Error err = ERROK;
 
 	err = bitsetinit(&bset, 8);
 
 	if(err != ERROK) {
 		printf("fail: bitset\n");
-		return;
+		return err;
 	}
 
 	if(bset.size != 1)
@@ -38,4 +45,75 @@ void main(void)
 
 cleanup:
 	bitsetfree(&bset);
+	return err;
+}
+
+u8 testLargeBitSet()
+{
+	Error err = ERROK;
+	u32 n = 10000000, i = 0;
+
+	BitSet bset;
+
+	err = bitsetinit(&bset, n);
+
+	if(err != ERROK) {
+		return err;
+	}
+
+	/* tests if all bits are zero'ed */
+	for(i = 0; i < n; i++) {
+		u8 v = bitsetget(&bset, i);
+
+		if(v != 0) {
+			err = 1;
+			goto cleanup;
+		}
+	}
+
+	/* test 1 bit flip */
+	bitsetset(&bset, 0);
+	u8 v = bitsetget(&bset, 0);
+
+	if(v != 1) {
+		err = 1;
+		goto cleanup;
+	}
+
+cleanup:	
+	bitsetfree(&bset);
+	return err;
+}
+
+void main(void)
+{
+	u8 i = 0, status = 0;
+	TestFn t1, t2;
+
+	t1.name = (u8 *) "testBasicBitSetOneByte";
+	t1.fn = testBasicBitSetOneByte;
+
+	t2.name = (u8 *) "testLargeBitSet";
+	t2.fn = testLargeBitSet;
+
+	tests[0] = t1;
+	tests[1] = t2;
+
+	for(i = 0; i < sizeof(tests)/sizeof(TestFn); i++)
+	{
+		TestFn t = tests[i];
+
+		printf("Running: %s: ", t.name);
+		u8 ret = t.fn();
+
+		if(ret != 0)
+		{
+			printf("fail\n");
+			status = ret;
+		} else {
+			printf("ok\n");
+		}
+	}
+
+	exit(status);
 }
